Bug fix with result showing (total generation calculation), adding data and plotting.

microgrid_full.py
@@ -38,30 +38,32 @@ minute_intervals = 15
 load_shifting = False
 # Price to be considered as "expensive" and thus worth it to discharge BESS/EV/load shift
 spot_price_following = False
-price_threshold = 0.05
+price_threshold = 1.00  # very high so that the SDN does not activate it
 
 # whether we try to optimise for self-consumption and self-sufficiency
 gen_shifting = False
 
 # whether we want fixed-price or spot-price (false)
-fixed = True
+fixed = False
 
 # Setting up the parameters of the microgrid components
 houses = 8
 # If we want to have the same household loads repeated for N houses.
 
 # PV system
-PV_installed_capacity = 50.0 #kWp
+PV_installed_capacity = 25.0 #kWp
 
 gen_data = gen_data * PV_installed_capacity #gen_data in kW
 
 # BESS
 BESS_parameters = {'capacity': 30.0, #capacity in kWh
                    'cRate': 3.0, #charge/discharge rate in kW
-                   'SoC': 30.0, # initial SoC
-                   'Control': 'price_threshold', #the type of control for the battery, will discharge when threshold is above setpoint
-                   'Control setpoint': price_threshold,
-                   'Grid enabled': True,
+                   'Initial SoC': 30.0, # initial SoC
+                   'SoC': 30.0, # variable SoC
+                   'Control': 'load_threshold',#'price_threshold', #the type of control for the battery, will discharge when threshold is above setpoint
+                   'Control setpoint': 5.00,#price_threshold,
+                   'Price threshold': price_threshold,
+                   'Grid enabled': False,
                    'SoC threshold': 1
                    }
 
@@ -128,7 +130,7 @@ load_list = [load1, load2, load3, load4, load5, load6, load7, load8]
 
 # Setting the load flexibility behaviour
 for load in load_list:
-    load.define_flexibility(0.15, 4, number_days, minute_intervals, plot=False)
+    load.define_flexibility(0.10, 4, number_days, minute_intervals, plot=False)
 
 
 
@@ -157,13 +159,13 @@ EV1 = cl.EV(EV_parameters['capacity'], EV_parameters['cRate'], EV_parameters['So
 EV1.set_EV_behaviour(number_days, final_time, minute_intervals, load_data, plot=False)
 
 EV2 = cl.EV(EV_parameters['capacity'], EV_parameters['cRate'], EV_parameters['SoC'], EV_parameters['discharge threshold'], EV_parameters['V2G'])
-EV2.set_EV_behaviour(number_days, final_time, minute_intervals, load_data, plot=False, random=True)
+EV2.set_EV_behaviour(number_days, final_time, minute_intervals, load_data, plot=False, random=False)
 
 EV3 = cl.EV(EV_parameters['capacity'], EV_parameters['cRate'], EV_parameters['SoC'], EV_parameters['discharge threshold'], EV_parameters['V2G'])
-EV3.set_EV_behaviour(number_days, final_time, minute_intervals, load_data, plot=False, random=True)
+EV3.set_EV_behaviour(number_days, final_time, minute_intervals, load_data, plot=False, random=False)
 
 EV4 = cl.EV(EV_parameters['capacity'], EV_parameters['cRate'], EV_parameters['SoC'], EV_parameters['discharge threshold'], EV_parameters['V2G'])
-EV4.set_EV_behaviour(number_days, final_time, minute_intervals, load_data, plot=False, random=True)
+EV4.set_EV_behaviour(number_days, final_time, minute_intervals, load_data, plot=False, random=False)
 
 EV_list = [EV1, EV2, EV3, EV4]
 
@@ -246,34 +248,161 @@ KPI_scss, KPI_econ = fn.mg_eval(microgrid_simulation, minute_intervals)
 
 # %% Calculating the flexibility availability
 
+# # we want to calculate how much up- and down-regulation is available from the Energy Community at each time-step.
+# # i.e., how much flexibility the SDN can aggregate and offer to frequency markets.
+
+# # We want to provide up-regulation, which is adding energy or reducing the load to increase the frequency; and down-regulation, which is 
+# # reducing the energy injected or increasing the load to reduce the frequency. Each asset in the microgrid will then behave differently in this context.
+
+# #### LOADS
+
+# # the maximum load possible at each time-step is calculated by the method Load.define_flexibility, and is represented in the parameter Load.maxload
+# # The maximum load is calculated as the sum of all possible load shifts from adjacent time-steps.
+# # Similarly, the minimum load is also calculated by the same method and expressed in Load.minload. It is the reduction of (%) of flexibility in each time-step.
+
+# # For the first two days and load 1, we have
+# plt.figure()
+# plt.plot(load1.maxload[0:192], linestyle='--', linewidth=0.7, label='maxload')
+# plt.plot(load1.load[0:192], label='Original load values')
+# plt.plot(load1.newload[0:192], label='Shifted load')
+# plt.plot(load1.minload[0:192], linestyle='--', linewidth=0.7, label='minload')
+# plt.legend()
+
+# # In this case, the maximum load possible is much higher than the load peak, so we'll certainly have extra peaks in the distribution grid if we shift 
+# # loads up until maxload of a time-step.
+
+# # The up-regulation potential from the load is calculated by the difference between the load at time-step (t) and the minimum load at time-step (t), i.e.,
+# # how much load is there still to be shifted away from this time-step.
+
+# up_reg_load = load1.newload - load1.minload
+# # And the down-regulation potential is the difference between the maximum possible load and the load at time (t), i.e., how much load can still be shifted to
+# # this time-step.
+# down_reg_load = load1.maxload - load1.newload
+
+# #%%
+# #### BESS
+
+# # For the BESS, up-regulation means delaying the charge (when charging from the grid), i.e. reducing its load, or discharging (when idle)
+
+# # When we have grid imports (<0), the upregulation from the BESS charging would be just its capacity to reduce load (its I/O). When we export to the grid, 
+# # there's no upregulation possible from the charging of the battery, as it's already charging at its limit because we prioritise BESS charging over grid export.
+ 
+# up_reg_bess_ch = np.where(microgrid_simulation['Grid import/export'].values < 0, microgrid_simulation['BESS charge/discharge'].values, 0)
+
+# # However, for up-regulation, we only want to consider the times when the BESS is charging, so we calculate how much charging can be delayed
+# up_reg_bess_ch = np.where(up_reg_bess_ch > 0, up_reg_bess_ch, 0) 
+
+# # We can also provide up-regulation when the BESS is discharging or idle.
+# # The discharge up-regulation is the difference between what CAN be discharged (min(SoC, cRate)) and what IS being discharged (negative BESS charge/discharge)
+
+# up_reg_bess_disch_capacity = np.min([microgrid_simulation['BESS_SoC'].values, BESS_parameters['cRate']*np.ones_like(microgrid_simulation['BESS_SoC'].values)], axis=0)
+
+# # the BESS discharge that is happening is
+# up_reg_bess_disch_happening = np.where(microgrid_simulation['BESS charge/discharge'] < 0, microgrid_simulation['BESS charge/discharge'], 0)
+
+# # However we must keep in mind that the BESS SoC is AT THE END of the timestamp, whereas the BESS I/O is DURING the timestamp.
+# # This means that for instance, the BESS was charged from 0kWh to 1kWh at between 11:00 - 11:15, the I/O column will have 
+# # 1kWh at 11:00 timestmap, with a 0kWh SoC; and at the timestamp 11:15 its SoC is now 1kWh. 
+# # Thus, we must "jump" the SoC values one index later to reflect the flexibility capacity that is available at that timestamp.
+# up_reg_bess_disch_capacity = np.roll(up_reg_bess_disch_capacity, 1)
+# up_reg_bess_disch_capacity[0] = min(BESS_parameters['Initial SoC'], BESS_parameters['cRate'])
+
+# # Now we can calculate how much available flexibility is there at each timestamp
+
+# up_reg_bess_disch = up_reg_bess_disch_capacity - abs(up_reg_bess_disch_happening)
+
+
+# # To check it
+
+# up_reg_bess = up_reg_bess_ch + up_reg_bess_disch
+
+# # Now we perform the dowm-regulation for the BESS. This means calculating how much extra load (charging or stopped discharging) can be added in each timestamp.
+
+# # How much can we increase charging
+# down_reg_bess_ch_capacity = np.min([BESS_parameters['capacity'] - microgrid_simulation['BESS_SoC'].values, 
+#                                     BESS_parameters['cRate']*np.ones_like(microgrid_simulation['BESS_SoC'].values)], axis=0)
+
+# down_reg_bess_ch_happening = np.where(microgrid_simulation['BESS charge/discharge'] > 0, microgrid_simulation['BESS charge/discharge'], 0)
+
+# down_reg_bess_ch_capacity = np.roll(down_reg_bess_ch_capacity, 1)
+# down_reg_bess_ch_capacity[0] = min(BESS_parameters['capacity'] - BESS_parameters['Initial SoC'], BESS_parameters['cRate'])
+
+# down_reg_bess_ch = down_reg_bess_ch_capacity + abs(down_reg_bess_ch_happening)
+
+
+
+# # How much can we reduce discharging (thus increasing the overall load) - it can be the WHOLE discharging when we have grid imports.
+# down_reg_bess_disch = np.where(microgrid_simulation['Grid import/export'].values < 0, microgrid_simulation['BESS charge/discharge'].values, 0)
+# down_reg_bess_disch = np.where(down_reg_bess_disch < 0, down_reg_bess_disch, 0)
+
+
+# down_reg_bess = down_reg_bess_ch + abs(down_reg_bess_disch)
+
+
+# BESS_regulation = pd.DataFrame()
+
+# BESS_regulation['BESS_SoC'] = microgrid_simulation['BESS_SoC']
+# BESS_regulation['BESS_io'] = microgrid_simulation['BESS charge/discharge']
+
+# BESS_regulation['up_charging avoidable']
+# BESS_regulation['up_discharge cap']
+# BESS_regulation['up_reg']
+# BESS_regulation['down_discharging avoidable']
+# BESS_regulation['down_charging cap']
+# BESS_regulation['down_reg']
+
+
+
+# BESS_regulation = pd.DataFrame({'UP - Discharge Capacity': up_reg_bess_disch_capacity, 
+#                                 'UP - Discharging happening': up_reg_bess_disch_happening,
+#                                 'UP - Discharge Potential': up_reg_bess_disch})
+
+# BESS_regulation['DOWN - regulation potential'] = down_reg_bess
+# BESS_regulation['UP - Charging happening avoidable'] = up_reg_bess_ch
+# BESS_regulation['UP - regulation potential'] = up_reg_bess
+# BESS_regulation['DOWN - Discharging happening avoidable'] = down_reg_bess_disch
+# BESS_regulation['DOWN - Charge Capacity'] = down_reg_bess_ch_capacity
+# BESS_regulation['DOWN - Charging Occurring'] = down_reg_bess_ch_happening
+# BESS_regulation['DOWN - Charging Potential'] = down_reg_bess_ch
 
 
 
 
+# up_reg_bess_disch = np.min([microgrid_simulation['BESS_SoC'].values, BESS_parameters['cRate']*np.ones_like(microgrid_simulation['BESS_SoC'].values)], axis=0)
+# up_reg_bess_disch = up_reg_bess_disch - abs(np.where(microgrid_simulation['BESS charge/discharge'] < 0, microgrid_simulation['BESS charge/discharge'], 0))
 
-
+# if BESS_parameters['Grid Enabled'] == True:
+#     # the BESS_io will be positive (charge) also in times when there's no excess generation 
+#     if BESS_io > 0 and : # it's charging
+#         BESS_ch_upreg = BESS_io
+# in each time-step
+# check if it is charging, discharging, or idle
+# if charging, check if it is from the grid, and if it is, it can be delayed
+#up_reg_BESS = BESS_charging
+# if discharging or idle, up-regulation capacity is limited only by c-Rate
+#up_reg_BESS = min(cRate, capacity)
 
 
 # %% Calculating the average loads
 
-time_intervals = pd.date_range(start='00:00', end='23:45', freq='15T').strftime('%H:%M')
-daily_values = pd.DataFrame(index=time_intervals)
-hourly_values = []
+# time_intervals = pd.date_range(start='00:00', end='23:45', freq='15T').strftime('%H:%M')
+# daily_values = pd.DataFrame(index=time_intervals)
+# hourly_values = []
 
-# for idx, row in daily_values.iterrows():
-#     mask = (microgrid_simulation.index.hour == idx.hour) & (microgrid_simulation.index.minute == idx.minute)
-#     hourly_values.append(microgrid_simulation.loc[mask, 'Total demand_shift'].values)
+# # for idx, row in daily_values.iterrows():
+# #     mask = (microgrid_simulation.index.hour == idx.hour) & (microgrid_simulation.index.minute == idx.minute)
+# #     hourly_values.append(microgrid_simulation.loc[mask, 'Total demand_shift'].values)
 
 
 
-for hour in range(24):
-    for minute in [00, 15, 30, 45]:
-        mask = (microgrid_simulation.index.hour == hour) & (microgrid_simulation.index.minute == minute)
-        #daily_values.loc[hour:minute, 'Values'] = microgrid_simulation.loc[mask, 'Total demand_shift'].values
-        #daily_values['Values'] = microgrid_simulation.loc[mask, 'Total demand_shift'].values
-        hourly_values.append(microgrid_simulation.loc[mask, 'Total demand_shift'].values)
+# for hour in range(24):
+#     for minute in [00, 15, 30, 45]:
+#         mask = (microgrid_simulation.index.hour == hour) & (microgrid_simulation.index.minute == minute)
+#         #daily_values.loc[hour:minute, 'Values'] = microgrid_simulation.loc[mask, 'Total demand_shift'].values
+#         #daily_values['Values'] = microgrid_simulation.loc[mask, 'Total demand_shift'].values
+#         hourly_values.append(microgrid_simulation.loc[mask, 'Total demand_shift'].values)
 
-daily_values['Values'] = hourly_values
+# daily_values['Values'] = hourly_values
 
 # %% 
 # fig, ax = plt.subplots()